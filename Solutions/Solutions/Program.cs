67951880f9b26c3e6f8d55d9f3e85007e0fa0e0b
Jason <15828589290@163.com>
2019-02-20 22:39:48 +0800

    update

---
+++ Solutions/Solutions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum Solution1 = new TwoSum();
+            int[] test = new int[] { 2, 5, 7, 11 };
+            int target = 9;
+            int[] res = Solution1.toTwoSum(test, target);
+            Console.WriteLine(res[0] + " " + res[1]);
+        }
+    }
+}
---
+++ Solutions/Solutions/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    class TwoSum
+    {
+        public int[] toTwoSum(int [] nums,int target)
+        {
+            List<int[]> result = new List<int[]>();
+            int[] aa = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && i != j)
+                    {
+                        aa[0] = i;
+                        aa[1] = j;
+                    }
+                }
+            }
+            return aa;
+        }
+    }
+}
