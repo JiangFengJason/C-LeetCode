bd6e62ae9e8ca7571d24a92509245a2ceb498b79
Jason <15828589290@163.com>
2019-02-22 20:49:56 +0800

    2.22

--- Solutions/Solutions/Program.cs
+++ Solutions/Solutions/Program.cs
@@ -12,22 +12,28 @@ namespace Solutions
             //int[] res = Solution1.ToTwoSum(test, target);
             //Console.WriteLine(res[0] + " " + res[1]);
 
-            AddTwoNumbers Solution2 = new AddTwoNumbers();
-            ListNode l1 = new ListNode(2);
-            l1.next = new ListNode(4);
-            l1.next.next = new ListNode(3);
+            //AddTwoNumbers Solution2 = new AddTwoNumbers();
+            //ListNode l1 = new ListNode(2);
+            //l1.next = new ListNode(4);
+            //l1.next.next = new ListNode(3);
 
-            ListNode l2 = new ListNode(5);
-            l2.next = new ListNode(6);
-            l2.next.next = new ListNode(4);
+            //ListNode l2 = new ListNode(5);
+            //l2.next = new ListNode(6);
+            //l2.next.next = new ListNode(4);
 
-            ListNode result = Solution2.ToAddTwoNumbers(l1, l2);
+            //ListNode result = Solution2.ToAddTwoNumbers(l1, l2);
 
-            while(result!=null)
-            {
-                Console.WriteLine(result.val);
-                result = result.next;
-            }
+            //while(result!=null)
+            //{
+            //    Console.WriteLine(result.val);
+            //    result = result.next;
+            //}
+
+            Reverse Solution3 = new Reverse();
+            int origin =1534236469;
+            Console.WriteLine(origin);
+            int result = Solution3.toReverse(origin);
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Solutions/Solutions/Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    class Reverse
+    {
+        public int toReverse(int x)
+        {
+            int res = 0;
+            while (x != 0)
+            {
+                int t = res * 10 + x % 10;
+                if ((t - x % 10) / 10 != res) return 0;
+                res = t;
+                x /= 10;
+            }
+            return res;
+        }
+    }
+}
