ac17cb9ab97e82d3bab279d5f71d1fcf6b853d6e
Jason <15828589290@163.com>
2019-03-17 21:27:39 +0800

    3.17

---
+++ Solutions/Solutions/MaximumSubarray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = 0;
+            int ans;
+            ans = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                ans = Math.Max(ans, sum);
+                sum = Math.Max(sum, 0);
+            }
+            return ans;
+        }
+    }
+}
