2bed1bcc98da8e48202b68fa0f831f9e8810c097
Jason <15828589290@163.com>
2019-03-18 21:47:21 +0800

    3.18

---
+++ Solutions/Solutions/RemoveNthNodeFromEndofList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    class RemoveNthNodeFromEndofList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode p = head;
+            ListNode q = head;
+            if (head.next==null) return null;
+            for (int i = 0; i < n; ++i) q = q.next;
+            if (q==null) return head.next;
+            while (q.next!=null)
+            {
+                q = q.next;
+                p = p.next;
+            }
+            p.next = p.next.next;
+            return head;
+        }
+    }
+}
