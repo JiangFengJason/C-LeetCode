86efa3b93869e219783f09176a82ec8f05a8ae75
Jason <15828589290@163.com>
2019-03-20 23:11:11 +0800

    3.20

---
+++ Solutions/Solutions/LengthofLastWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    class LengthofLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            int lastIndex = s.LastIndexOf(' ') + 1;
+            return s.Length - lastIndex;
+        }
+    }
+}
