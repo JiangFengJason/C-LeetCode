d336feac645521418d4e66c4a1b541ee6b3a2337
Jason <15828589290@163.com>
2019-03-25 20:54:45 +0800

    3.25

---
+++ SolutionsPY/PlusOne.py
@@ -0,0 +1,12 @@
+import math
+
+class Solution:
+    def plusOne(self, digits):
+        sum=0
+        res=[]
+        for i in range(len(digits)):
+            sum=sum+digits[i]*pow(10,(len(digits)-i-1))
+        sum+=1
+        for i in range(len(str(sum))):
+            res.append(int(str(sum)[i]))
+        return res
---
+++ SolutionsPY/main.py
@@ -0,0 +1,6 @@
+from PlusOne import Solution
+
+digit=[1,0,0,0,0]
+PlusOne=Solution()
+res=PlusOne.plusOne(digit)
+print(res)
