5a9a979ab77f4a88567a0576189584e18182b251
Jason <15828589290@163.com>
2019-10-12 16:08:26 +0800

    10.12

---
+++ SolutionsPY/LowestCommonAncestorofaBinarySearchTree.py
@@ -0,0 +1,12 @@
+from SameTree import TreeNode
+class LowestCommonAncestorofaBinary:
+    def lowestCommonAncestor(self, root, p, q):
+        parent_val=root.val
+        p_val=p.val
+        q_val=q.val
+        if p_val>parent_val and q_val>parent_val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        elif p_val<parent_val and q_val<parent_val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        else:
+            return root
