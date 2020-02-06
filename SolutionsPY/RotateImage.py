c99ec9d82a86091c7737b554e07763b3167340e8
jason <15828589290@163.com>
2020-02-06 16:40:30 +0800

    2.6

---
+++ SolutionsPY/RotateImage.py
@@ -0,0 +1,9 @@
+class RotateIma:
+    def rotate(self, matrix):
+        n = len(matrix[0])
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        
+        for i in range(n):
+            matrix[i].reverse()
